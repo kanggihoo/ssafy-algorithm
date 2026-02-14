1e7299ba21f826c596130087a2a393bf8be4fb20
kkh <11kkh19999@gmail.com>
2026-02-14 15:56:09 +0900

    새로운 java 프로젝트 추가

---
+++ SWEA/src/D6_1855.java
@@ -0,0 +1,17 @@
+// https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5LnipaDvwDFAXc
+//영준이는 루트가 있는 트리에서 BFS(Breadth First Search)를 하려고 한다.
+//
+//트리는 1에서 N까지의 번호가 붙은 N개의 노드로 이루어져 있으며, 1이 루트이자 동시에 탐색 시작점이다.
+//
+//BFS는 큐를 이용하여 탐색을 하는데, 큐의 가장 앞에 있는 노드를 뽑아 탐색을 하고, 탐색을 하는 노드의 자식들을 작은 번호부터 순서대로 큐의 뒤쪽에 넣는 방식으로 탐색이 진행된다.
+//
+//이것은 컴퓨터에서 실제로 진행되는 방식이 아니고 영준이가 직접 노드를 방문해야 하기 때문에, BFS를 한다면 노드를 방문하는 순서가 정해질 것이고 영준이는 그 순서를 따라 최단거리로 트리를 이동하여 모든 노드를 탐색한다.
+//
+//영준이는 과연 몇 개의 간선을 지나고 나서야 탐색을 끝 마칠 수 있을까?
+public class D6_1855 {
+	
+	public static void main(String[] args) {
+		System.out.print("sds");
+	}
+
+}
