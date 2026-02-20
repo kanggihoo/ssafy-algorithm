d31785dad223a119b2b4d510d5401d29c44eedcf
kkh <11kkh19999@gmail.com>
2026-02-20 17:45:30 +0900

    P14596 섬지키기 문제 풀이완료 , 다른 사람에 비해 실행시간이 김, 최적화 필요해보임

--- SW-Btype/src/P14596_섬지키기/UserSolution.java
+++ SW-Btype/src/P14596_섬지키기/UserSolution.java
@@ -6,14 +6,22 @@ import java.util.List;
 class UserSolution
 {
 	class Structer{
-		int[] r;
-		int[] c;
-		int[] h;
+		int dx;
+		int dy;
+		int d;
+		int[] v;
 		
-		public Structer(int M) {
-			r = new int[M];
-			c = new int[M];
-			h = new int[M];
+		// y:1이면 세로방향 , x:1이면 가로 방향블록 , d:0이면 정방향, 1이면 반대방향
+		public Structer(int M , int y , int x , int d , int[] S) {
+			this.dy = y;
+			this.dx = x;
+			this.d = d;
+			v = new int[M];
+			if(d==0) {
+				for(int i = 0 ; i < M ; i++) v[i] = S[i];
+			}else {
+				for(int i = 0 ; i < M ; i++) v[i] = S[M-1-i];
+			}
 		}
 			
 	}
@@ -21,7 +29,7 @@ class UserSolution
 	public int N;
 	public int[][] V;
 	public int vCnt;
-	public int[] dy= {0,1,0,1,-1};
+	public int[] dy= {0,1,0,-1};
 	public int[] dx= {1,0,-1,0};
 	public void init(int N, int mMap[][])
 	{
@@ -32,86 +40,159 @@ class UserSolution
 		V = new int[N][N];
 		
 	}
-	// �������� ��� �����ϰų� ȸ���ص� �������� Ȯ���� �ʿ���.ȸ���ϴ°� �ǹ̾��� ��� �ߺ����� ���� ����. 
+	//  
 	public int numberOfCandidate(int M, int mStructure[])
 	{
-		Structer[] Shape = new Structer[4];
-		for(int i = 0 ; i < 4 ; i++) Shape[i] = new Structer(M);
+		 
+		List<Structer> Shape = generateShape(M , mStructure);
 		
-		for(int j = 0 ; j<M ; j++) {
-			Shape[0].r[j] = 0;
-			Shape[0].c[j] = j;
-			Shape[0].h[j] = mStructure[j];
-			
-			Shape[1].r[j] = j;
-			Shape[1].c[j] = 0;
-			Shape[1].h[j] = mStructure[j];
-			
-			Shape[2].r[j] = 0;
-			Shape[2].c[j] = j;
-			Shape[2].h[j] = mStructure[M-1-j];
-			
-			Shape[3].r[j] = j;
-			Shape[3].c[j] = 0;
-			Shape[3].h[j] = mStructure[M-1-j];
+		// 설치 할 수 있는 구조물 개수 구하기 
+		int ans =0;
+		
+		for(int i = 0 ; i < N ; i++) {
+			for(int j = 0 ; j < N ; j++) {
+				for(Structer s : Shape) {
+					
+					if(s.dy == 1 && i+M>N) continue; // 세로 방향 구조물 놓을 수 있는지
+					else if(s.dx == 1 && j+M>N) continue; // 가로 방향 구조물 놓을 수 있는지
+					// 현재 구조물 놓을 수 있고 합이 동일한지 
+					if(isPlace(i,j,s)) ans++;
+				}
+			}
 		}
 		
-		// ���� ��翡�� �ߺ� ���� �� ���� ��ȯ�ϴ� Structer[] �� ��ȯ�ϴ°��� �Լ�ȭ �ϱ� 
-		
-		
-			
-		return 0;
+		return ans;
 	}
+	
 
+	// 해수면이 mSeaLevl 만큼 상승해도 바다에 잠기지 않고 남는 지역 개수 최대가 되도록 구조물 설치
 	public int maxArea(int M, int mStructure[], int mSeaLevel)
 	{
-		// �������� ���� �ؼ��� ��� ���Ѿ� �ϴµ� 
-		// ���� ���� 
-	
-		// ���� ���� 
+		List<Structer> Shape = generateShape(M , mStructure);
 		
-		
-		
-		// �� ���̵� ���� 
-		vCnt ++;
+		// 설치 할 수 있는 구조물 개수 구하기 
+		int cnt =0;
+		int ans = 0;
 		for(int i = 0 ; i < N ; i++) {
-			if(map[0][i] > mSeaLevel-1) continue;
-			
-			dfs(0,i,mSeaLevel-1);
+			for(int j = 0 ; j < N ; j++) {
+				for(Structer s : Shape) {
+					if(s.dy == 1 && i+M>N) continue; // 세로 방향 구조물 놓을 수 있는지
+					else if(s.dx == 1 && j+M>N) continue; // 가로 방향 구조물 놓을 수 있는지
+					// 현재 구조물 놓을 수 있고 합이 동일한지 
+					if(isPlace(i,j,s)) {
+						cnt ++;
+						place(i,j,s,1);
+						ans = Math.max(ans , getArea(mSeaLevel));
+						place(i,j,s,-1);
+					}
+				}
+			}
 		}
-		
-		for(int j = 1 ; j<N ; j++) {
-			
-		}
+		if(cnt == 0) return -1;
 		
 		
-		
-		
-		return 0;
+		return ans;
 	}
 	
-	public boolean check(int r,int c, int d , int[] S ) {
-		int M = S.length -1;
-		int ref = map[r][c]+S[0];
-		int ny = r+dy[d];
-		int nx = c+dx[d];
-		if(d >= 2) {
-			ny = r-dy[d]*(M);
-			nx = c-dx[d]*(M);
-			ref = map[ny][nx] + S[0];
-			ny +=dy[d];
-			nx +=dx[d];
+	public List<Structer> generateShape(int M , int[] mStructure){
+		// 현재 구조물이 대칭인지 확인 후 가능한 조합 확인 
+		List<Structer> Shape = new ArrayList<>();
+		if(M==1) {
+			Shape.add(new Structer(M, 1, 0, 0, mStructure));
 		}
-		for(int i = 0 ;  i<M; i++) {
-			if(map[ny][nx]+S[i] != ref) return false;
-			ny+=dy[d];
-			nx+=dx[d];
+		else if(isSame(M , mStructure)) {
+			Shape.add(new Structer(M, 0, 1, 0, mStructure));
+			Shape.add(new Structer(M, 1, 0, 0, mStructure));
+		}else {
+			Shape.add(new Structer(M, 0, 1, 0, mStructure));
+			Shape.add(new Structer(M, 1, 0, 0, mStructure));
+			
+			Shape.add(new Structer(M, 0, 1, 1, mStructure));
+			Shape.add(new Structer(M, 1, 0, 1, mStructure));
+		}
+		
+		return Shape;
+	}
+	
+	public boolean isPlace(int y , int x , Structer s) {
+		int[] sV = s.v;
+		int res = s.v[0]+map[y][x];
+		if(s.dy == 1) { // 세로방향
+			for(int i = 1 ; i <sV.length ; i++) {
+				if(res != s.v[i]+map[y+i][x]) return false;
+			}
+		}else if(s.dx == 1) { // 가로 방향
+			for(int i = 1 ; i <sV.length ; i++) {
+				if(res != s.v[i]+map[y][x+i]) return false;
+			}
 		}
 		return true;
 	}
 	
-//	public void place( )
-	public void dfs(int r , int c , int t) {
+	public boolean isSame(int size , int[] S) {
+		for(int i = 0 ; i < size/2 ; i++) {
+			if(S[i] != S[size-1-i]) return false;
+		}
+		return true;
+	}
+	
+	public void place(int y, int x , Structer s , int val ) {
+		if(s.dy ==1) { // 세로방향
+			for(int i = 0 ; i < s.v.length ; i++) {
+				map[y+i][x] += val*s.v[i];
+			}
+		}else { // 가로 방향
+			for(int i = 0 ; i < s.v.length ; i++) {
+				map[y][x+i] += val*s.v[i];
+			}
+		}
+		
+	}
+	public int getArea(int level) {
+		// 양 사이드 조사해서 dfs 돌리기
+		int y = 0; 
+		int x = 0;
+		int d = 0;
+		
+		vCnt++;
+		int ans = N*N;
+		for(int i = 0 ; i < 4*N-4 ; i++) {
+			// 현재 위치에서 진행하고
+			if(map[y][x] < level && V[y][x] != vCnt) {
+				ans -= dfs(y,x,level);
+			}
+			
+			// 다음 위치 업데이트
+			int ny = y+dy[d];
+			int nx = x+dx[d];
+			
+			if(ny<0 || ny>=N || nx<0 || nx>=N) {
+				d++;
+				ny = y+dy[d];
+				nx = x+dx[d];
+			}
+			
+			y = ny;
+			x = nx;
+		}
+		
+		
+		return ans;
+	}
+	
+	public int dfs(int y, int x , int level) {
+		
+		V[y][x] = vCnt;
+		int res = 1;
+		for(int i = 0 ; i < 4 ; i++) {
+			int ny = y+dy[i];
+			int nx = x+dx[i];
+			if(ny<0 || ny >=N || nx<0 || nx>=N) continue;
+			if(map[ny][nx]< level && V[ny][nx] != vCnt) {
+				res += dfs(ny,nx,level);
+			}
+		}
+		return res;
 		
 	}
 }
