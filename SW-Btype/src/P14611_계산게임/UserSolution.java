2f63493a17931b10b6e54d0b28f8ec0d9eace572
kkh <11kkh19999@gmail.com>
2026-03-08 18:24:45 +0900

    계산게임 풀이완료

--- SW-Btype/src/P14611_계산게임/UserSolution.java
+++ SW-Btype/src/P14611_계산게임/UserSolution.java
@@ -1,4 +1,8 @@
 package P14611_계산게임;
+
+
+import java.util.*;
+
 /*
 카드 나열 하고 인접한 카드의 값을 이용하여 계산하는 게임
 카드는 두종류 (일반, 조커)
@@ -15,18 +19,102 @@ package P14611_계산게임;
 
 조커값은 게임중간마다 변경가능하고 , 가장 최근에 변경된 조커값을 사용
 
- */
+*/
+
 class UserSolution {
+    int[] table;
+    Deque<Integer>[][] D;
+    int l; int r;
+    int jV;
+    int MOD = 20;
+
+
 
     // 처음 테이블에 나열할 5장 카드(무조건 5장) (왼->오)
     // 조커에서 사용할 값이 주어짐.
     void init(int mJoker, int mNumbers[]) {
+        table = new int[100005];
+
+        // D 초기화
+        D = new ArrayDeque[5][20];
+        for(int i = 0 ; i <5 ; i++){
+            for(int j = 0 ; j < 20 ; j++){
+                D[i][j] = new ArrayDeque<>();
+            }
+        }
+        // 초기 위치 초기화
+        r = 50000;
+        l = 49999;
+        jV = mJoker;
+        // 처음 제공되는 5개 카드 초기화 및 누적합 구하기
+        for (int i = 0; i < 5; i++) {
+            int curNum = mNumbers[i];
+            table[r+i] = curNum;
+        }
+        // D 업데이트
+        for(int i = r ; i <r+2 ; i++){
+            int jCnt = 0;
+            int sum = 0;
+            for(int j = 0 ; j < 4 ; j++){
+                if(table[i+j] == -1){
+                    jCnt++;
+                }else {
+                    sum+=table[i+j];
+                }
+            }
+            D[jCnt][sum%MOD].offer(i);
+        }
+        r+=5;
     }
 
     // 추가로 나열할 5장의 카드(무조건5) 가 주어짐.(왼->오)
     // mDir : 0 => 현재 카드의 왼쪽에 추가 , 1: => 가장 오른쪽에 추가
     // 호출 횟수 : 10000회
+    void updateSum(int mDir , int[] mNumbers){
+        if (mDir == 0) { // 왼쪽에 추가
+            //누적합 및 D 업데이트
+            for (int i = 0; i < 5; i++) {
+                int curNum = mNumbers[5-i-1];
+                table[l-i] = curNum;
+            }
+            // D 업데이트
+            for(int i = l ; i>l-5 ; i--) {
+                int jCnt =0;
+                int sum=0;
+                for(int j = 0 ; j < 4 ; j++) {
+                    if(table[i+j] == -1) {
+                        jCnt++;
+                    }else {
+                        sum += table[i + j];
+                    }
+                }
+                D[jCnt][sum%MOD].offerFirst(i);
+            }
+            l-=5;
+        } else { // 오른쪽에 추가
+            for (int i = 0; i < 5; i++) {
+                int curNum = mNumbers[i];
+                table[r+i] = curNum;
+            }
+            // D 업데이트
+            for(int i = r-3 ; i<r+2 ; i++) {
+                int jCnt =0;
+                int sum=0;
+                for(int j = 0 ; j < 4 ; j++) {
+                    if(table[i+j] == -1) {
+                        jCnt++;
+                    }else {
+                        sum += table[i + j];
+                    }
+                }
+                D[jCnt][sum%MOD].offer(i);
+            }
+            r+=5;
+        }
+
+    }
     void putCards(int mDir, int mNumbers[]) {
+        updateSum(mDir , mNumbers);
     }
 
     // 인접한 카드 계산 결과가 mNum인 mNth 번째 카드 찾고, 그 카드와 오른쪽 3개 카드를 ret에 기록
@@ -35,10 +123,51 @@ class UserSolution {
     //    mNum : 찾아야 하는 계산 결과값 (0 ≤ mNum ≤ 19)
     //    mNth : 찾아야 하는 결과값이 나와야 하는 횟수 (1 ≤ mNth ≤ 1,000)
     int findNumber(int mNum, int mNth, int ret[]) {
-        return -1;
+        List<Deque> dqList = new ArrayList<>();
+        int total=0;
+        for(int jCnt = 0 ; jCnt<5 ; jCnt++){
+            int requiredV = (MOD+mNum-(jCnt*jV)%MOD)%MOD;
+            Deque<Integer> dq = D[jCnt][requiredV];
+            if(dq.size() >0){
+                total+=dq.size();
+                dqList.add(dq);
+            }
+
+        }
+        if(total < mNth){
+            return 0;
+        }
+        // dqList 순환하면서 mNth 번째 인덱스 찾기
+        PriorityQueue<int[]> pq = new PriorityQueue<>((e1,e2)->Integer.compare(e1[0] , e2[0]));
+        Iterator<Integer>[] iterators = new Iterator[dqList.size()];
+
+        // 각 dq의 첫번째 원소값 빼기
+        for(int i = 0 ; i < dqList.size() ; i++){
+            Iterator<Integer> it = dqList.get(i).iterator();
+            iterators[i] = it;
+            pq.offer(new int[]{it.next(),i});
+        }
+
+        // mTh번 반복
+        int resIdx = 0;
+        for(int i = 0 ; i<mNth ; i++){
+            int[] current = pq.poll();
+            resIdx = current[0];
+            if(iterators[current[1]].hasNext()){
+                pq.offer(new int[]{iterators[current[1]].next(), current[1]});
+            }
+        }
+        // ret 배열 업데이트
+        for(int i = 0 ; i <4 ; i++){
+            ret[i] = table[resIdx+i];
+        }
+        return 1;
+
     }
 
     // 조커 값을 mValue로 변경
     void changeJoker(int mValue) {
+        jV= mValue;
     }
 }
+
--- SWEA/src/P2383_점심식사시간_모의.java
+++ SWEA/src/P2383_점심식사시간_모의.java
@@ -1,6 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 /*
 NxN 방 , 점심을 먹기 위해 아래 층으로 최대한 빠르게 내려가야함.
 방안의 사람은 P , 계단 입구를 S라고 할때 이동완료 시간은 모든 사람이 계단을 내려가고, 아래 층으로 이동완료한 시간
@@ -26,7 +29,6 @@ NxN 방 , 점심을 먹기 위해 아래 층으로 최대한 빠르게 내려가
  계단에서 대기 하는 대기 큐 (계단 도착시간, 계단대기 시간 1초 고려해서) 
  그리고 계단도 큐에 넣고 해야하나?(진입시간+K)로 넣고, 현재 시간이 K가 되면 빼는 식으로?
  */
-import java.util.*;
 public class P2383_점심식사시간_모의 {
 
 	public static void main(String[] args) throws IOException {
@@ -37,77 +39,15 @@ public class P2383_점심식사시간_모의 {
         
         for(int t = 1 ; t<=T ; t++){
             st = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(st.nextToken()); // 방 한변길이 
-            List<Integer>people = new ArrayList<>();
-            List<int[]>stair = new  ArrayList<>();
-            
-            for(int i = 0 ; i < N ; i++) {
-            	st = new StringTokenizer(br.readLine());
-            	for(int j =0 ; j<N;j++) {
-            		int n = Integer.parseInt(st.nextToken()); 
-            		if(n == 1) {
-            			people.add(i*N+j);
-            		}else if(n >1) {
-            			stair.add(new int[] {i*N+j , n}); // (위치 , 계단길이)
-            		}
-            	}
-            }
-            
-            // 사람이 위치할 계단 위치 구하기 (0이면 0번 계단 , 1이면 1번 계단)  
-            int pCnt = people.size();
-            int ans = Integer.MAX_VALUE;
-            for(int i = 0 ; i < (1<<pCnt) ; i++) {
-            	
-            	Queue<Integer> s0 = new PriorityQueue<>((e1,e2)->Integer.compare(e1, e2));
-            	Queue<Integer> s1 = new PriorityQueue<>((e1,e2)->Integer.compare(e1, e2));
-            	
-            	for(int j = 0 ; j < pCnt ; j++) {
-            		int py = people.get(j) / N;
-            		int px = people.get(j) % N;
-            		if((i&(1<<j)) == 0) { // 0번계단
-            			int sy = stair.get(0)[0] / N;
-            			int sx = stair.get(0)[0] % N;
-            			s0.offer(Math.abs(py-sy)+Math.abs(px-sx));
-            		}else { // 1번계단
-            			int sy = stair.get(1)[0] / N;
-            			int sx = stair.get(1)[0] % N;
-            			s1.offer(Math.abs(py-sy)+Math.abs(px-sx));
-            		}
-            	}            	
-            	int res1 = simulation(s0 , stair.get(0)[1]);
-            	int res2 = simulation(s1 , stair.get(1)[1]);
-            	ans = Math.min(ans, Math.max(res1, res2));
+            int N = Integer.parseInt(st.nextToken()); // 셀 개수 
+            int M = Integer.parseInt(st.nextToken()); // 격리 시간
+            int K = Integer.parseInt(st.nextToken());  // 군집 개수
 
-            }
 
-            sb.append("#").append(t).append(" ").append(ans).append("\n");
+//            sb.append("#").append(t).append(" ").append(ans).append("\n");
         }
 
         System.out.print(sb);
     }
-	
-	public static int simulation(Queue<Integer> s , int stairSize) {
-		Queue<Integer> q = new ArrayDeque<Integer>();
-		while(!s.isEmpty()) {
-    		int enter = s.poll();
-    		if(q.size()< 3) {
-    			q.offer(enter+1+stairSize);
-    		}else {
-    			int pop = q.poll();
-    			if(pop <= enter) {
-    				q.offer(enter+1+stairSize);
-    			}else {
-    				q.offer(pop+stairSize);
-    			}
-    		}
-    	}
-    	
-    	// 큐에서 가장 큰 원소 반환 
-    	int res = 0;
-    	while(!q.isEmpty()) {
-    		res = Math.max(res, q.poll());
-    	}
-    	return res;
-	}
 
 }
