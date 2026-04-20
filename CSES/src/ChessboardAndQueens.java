3622132837498f2b2667239ee263e8ac1d94eaba
강기호 <11kkh19@naver.com>
2026-04-20 17:12:31 +0900

    0420 알고리즘 풀이 완료

---
+++ CSES/src/ChessboardAndQueens.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class ChessboardAndQueens {
+	static boolean[][] map;
+	static int ans;
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = 8;
+        StringBuilder sb = new StringBuilder();
+        map = new boolean[N][N];
+        
+        for(int i = 0 ; i < N ; i++) {
+        	int col = 0;
+        	for(char c : br.readLine().toCharArray()) {
+        		if(c=='*') map[i][col] = true;
+        		col++;
+        	}
+        }
+        ans =0;
+        dfs(0,0,0,0);
+        System.out.print(ans);
+    }
+	
+	public static void dfs(int row , int cS , int d1S , int d2S) {
+		if(row == 8) {
+			ans++;
+			return;
+		}
+		
+		// col 선택
+		for(int col = 0 ; col < 8 ; col++) {
+			if(map[row][col] || (cS & 1<<col)!=0 || (d1S & 1<<row-col+7)!=0 || (d2S & 1<<row+col) !=0 ) continue;
+			dfs(row+1 , cS | 1<<col , d1S | 1<<row-col+7 , d2S | 1<<row+col);
+		}
+	}
+
+}
---
+++ CSES/src/MexGridConstruction.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MexGridConstruction {
+	
+	public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        int[][] map = new int[N][N];
+        
+        // 초기화
+        for(int i = 0 ; i < N ; i++) {
+        	map[0][i] = i;
+        	map[i][0] = i;
+        }
+        
+        for(int i = 1 ; i < N ; i++) {
+        	for(int j = 1 ; j < N ; j++) {
+        		// (i,j) 의 숫자 결정하기
+        		boolean[] V = new boolean[N*N];
+        		for(int r = 0 ; r<i ; r++) {
+        			V[map[r][j]] = true;
+        		}
+        		
+        		for(int c = 0 ; c<j ; c++) {
+        			V[map[i][c]] = true;
+        		}
+        		int target = -1;
+        		for(int t = 0 ; t < V.length ; t++) {
+        			if(!V[t]) {
+        				target = t;
+        				break;
+        			}
+        		}
+        		
+        		map[i][j] = target;
+        	}
+        }
+        
+        // 출력 
+        for(int i = 0 ; i < N ; i++) {
+        	for(int j = 0 ; j < N ; j++) {
+        		sb.append(map[i][j]).append(" ");
+        	}
+        	sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+}
