0f4a65dc573890b407b82e5edefa3c58418fd8ca
kkh <11kkh19999@gmail.com>
2026-08-21 00:46:44 +0900

    sync

--- cpp/빙하의이동(D5)/solution.cpp
+++ cpp/빙하의이동(D5)/solution.cpp
@@ -24,14 +24,64 @@ struct RESULT
 {
 	int heights[MAX_N][MAX_N];
 };
+// 그 빙하가 움직이는 방향 ( 0 : ↑, 1 : →, 2 : ↓, 3 : ←) 을 의미한다.
 
+struct ICE
+{
+	int v;
+	int area;
+	int y;
+	int x;
+};
+
+int dy[4] = {-1,0,1,0};
+int dx[4] = {0,1,0,-1};
+
+int (*curH)[MAX_N];
+int (*curP)[MAX_N];
+bool tmp[MAX_N][MAX_N];
 void init(int N, int M, int mIceBlock[MAX_N][MAX_N], int mIceGroup[MAX_M][3])
 {
+	curH = mIceBlock;
+	
+	// int mIceGroup[MAX_M][3] 여기에서 제공된 (x,y)가 나중에 높이가 0이 될수도 있어서 미리 모든 위치에 대한 이동방향 결정해주어야함.
+	for(int i = 0 ; i < MAX_N ; i++){
+		for(int j = 0 ; j< MAX_N ; j++){
+			tmp[i][j] = false;
+		}
+	}
+
 
 }
 
 RESULT res;
 RESULT oneYearLater()
 {
+	// 1. 융해
+	for(int i = 0 ; i < MAX_N ; i++){
+		for(int j = 0 ; j< MAX_N ; j++){
+			for(int d = 0 ; d <4 ; d++){
+				if(curH[i][j] <=0) continue;
+				int ny = i+dy[d]; int nx = j+dx[d];
+				if(ny < 0 || ny >= MAX_N || nx < 0 || nx >=MAX_N) continue;
+
+				if(curH[ny][nx] ==0){
+					tmp[ny][nx] = true;
+					break;
+				}
+			}
+		}
+	}
+
+	// 일괄로 높이 감소 
+	for(int i = 0 ; i< MAX_N ; i++){
+		for(int j = 0 ; j< MAX_N ; j++){
+			if(tmp[i][j]) curH[i][j]--;
+		}
+	}
+
+	// 2. 이동
+
+	// 3. 병합 
 	return res;
 }
